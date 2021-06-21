600f7b275a6c677031f14bb2486a30b87de5241b
raintype <raintype@nexon.co.kr>
2021-06-21 15:13:13 +0900

    'init'

---
+++ src/test/java/com/community/community/CommunityApplicationTests.java
@@ -0,0 +1,51 @@
+package com.community.community;
+
+//import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+//import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Value("${property.test.name}")
+    private String propertyTestName;
+
+    @Value("${propertyTest}")
+    private String propertyTest;
+
+    @Value("${nokey:default value}")
+    private String defaultValue;
+
+    @Value("${propertyTestList}")
+    private String[] propertyTestArray;
+
+    @Value("#{'${propertyTestList}'.split(',')}")
+    private List<String> propertyTestList;
+
+    @Test
+    public void testValue() {
+        assertThat(propertyTestName).isEqualTo("property depth test");
+        assertThat(propertyTest).isEqualTo("test");
+        assertThat(defaultValue).isEqualTo("default value");
+
+        assertThat(propertyTestArray[0]).isEqualTo("a");
+        assertThat(propertyTestArray[1]).isEqualTo("b");
+        assertThat(propertyTestArray[2]).isEqualTo("c");
+
+        assertThat(propertyTestList.get(0)).isEqualTo("a");
+        assertThat(propertyTestList.get(1)).isEqualTo("b");
+        assertThat(propertyTestList.get(2)).isEqualTo("c");
+
+    }
+
+}
